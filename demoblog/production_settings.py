1c558e5693e78f8dc440f78596d77ad53b2f97b1
Osaetin Daniel <osaetindaniel@gmail.com>
2017-03-27 18:09:25 +0100

    Updated production_settings removed article folder

--- demoblog/production_settings.py
+++ demoblog/production_settings.py
@@ -1,5 +1,16 @@
+import os
 from .settings import *
 
-debug = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'danidee.pythonanywhere.com']
+
+# email config
+DEFAULT_FROM_EMAIL = 'osaetindaniel@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'osaetindaniel@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAILPASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
